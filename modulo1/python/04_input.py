7f1b2f2794a4c057333cb5bb989a65d779f8fca5
Kevin Diaz R <kevidiaz8@gmail.com>
2026-04-08 19:56:33 -0500

    08-04-2026

---
+++ modulo1/python/04_input.py
@@ -0,0 +1,7 @@
+nombre=input("Cual es su nombre?")
+print(f"Hola, {nombre}")
+
+edad_str=input("Ingrese edad? ")
+print(f"Tienes, {edad_str} años")
+edad=int(edad_str)
+print(f"El dible de tu edad es, {edad_str*2} años")
